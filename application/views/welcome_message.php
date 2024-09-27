0563a5f3954c294c3ca367d2bd1661d87bc300ef
alifbenarrivo <141648843+alifbenarrivo@users.noreply.github.com>
2024-09-27 19:40:48 +0800

    Ucapan

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to DeKontrak</title>
+	<title>Selamat Datang Di DeKontrak</title>
 
 	<style type="text/css">
 
